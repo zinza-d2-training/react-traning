46562f991501f03bad4cfd72769dfe58f91aa5f9
tuannd <tuan.nd@zinza.com.vn>
2022-02-09 11:31:26 +0700

    Set up Github CI, eslint check, install material-ui

---
+++ .github/pull_request_template.md
@@ -0,0 +1,13 @@
+## Summary
+
+<!-- Briefly describe your changes. -->
+
+## Dependencies
+
+<!-- Does this have any dependencies with Hasura or Lambda? If so, link related PRs. -->
+
+–
+
+## Testing
+
+<!-- Outline a testing plan. Any specific items you want a reviewer to focus on? -->
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {getUser} from './utils/auth'
-import routes from './routes'
-import {Switch, BrowserRouter} from "react-router-dom";
+import { getUser } from './utils/auth';
+import routes from './routes';
+import { Switch, BrowserRouter } from 'react-router-dom';
 
 function App() {
+  //Create Auth Context to check login, redirect to login if not logged in, redirect to home if logged in
   return (
     <BrowserRouter>
-      <Switch>
-        {routes}
-      </Switch>
+      <Switch>{routes}</Switch>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
